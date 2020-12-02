069c9cafa66b75439064a12334624141152fb651
chentong1023 <chentong1023@qq.com>
2020-12-02 15:04:24 +0800

    stage2vae has done

--- eval.py
+++ eval.py
@@ -15,7 +15,6 @@ from dataset import load_dataset, load_test_dataset
 
 
 def main():
-    tf.reset_default_graph()
     # exp info
     exp_folder = os.path.join(args.output_path, args.dataset, args.exp_name)
     if not os.path.exists(exp_folder):
@@ -24,74 +23,22 @@ def main():
     if not os.path.exists(model_path):
         os.makedirs(model_path)
 
-    # dataset
-    x, side_length, channels = load_dataset(args.dataset, args.root_folder)
-    input_x = tf.placeholder(tf.uint8, [args.batch_size, side_length, side_length, channels], 'x')
-    num_sample = np.shape(x)[0]
-    print('Num Sample = {}.'.format(num_sample))
-
     # model
-    if args.network_structure != 'Resnet':
-        model = eval(args.network_structure)(input_x, args.latent_dim, args.second_depth, args.second_dim, args.cross_entropy_loss)
-    else:
-        model = Resnet(input_x, args.num_scale, args.block_per_scale, args.depth_per_block, args.kernel_size, args.base_dim, args.fc_dim, args.latent_dim, args.second_depth, args.second_dim, args.cross_entropy_loss)
-    sess = tf.InteractiveSession()
-    sess.run(tf.global_variables_initializer())
-    writer = tf.summary.FileWriter(exp_folder, sess.graph)
-    saver = tf.train.Saver()
-
-    # train model
-    iteration_per_epoch = num_sample // args.batch_size 
-    if not args.val:
-        # first stage
-        for epoch in range(args.epochs):
-            np.random.shuffle(x)
-            lr = args.lr if args.lr_epochs <= 0 else args.lr * math.pow(args.lr_fac, math.floor(float(epoch) / float(args.lr_epochs)))
-            epoch_loss = 0
-            for j in range(iteration_per_epoch):
-                image_batch = x[j*args.batch_size:(j+1)*args.batch_size]
-                loss = model.step(1, image_batch, lr, sess, writer, args.write_iteration)
-                epoch_loss += loss 
-            epoch_loss /= iteration_per_epoch
-
-            print('Date: {date}\t'
-                  'Epoch: [Stage 1][{0}/{1}]\t'
-                  'Loss: {2:.4f}.'.format(epoch, args.epochs, epoch_loss, date=time.strftime('%Y-%m-%d %H:%M:%S')))
-        saver.save(sess, os.path.join(model_path, 'stage1'))
-
-        # second stage
-        mu_z, sd_z = model.extract_posterior(sess, x)
-        idx = np.arange(num_sample)
-        for epoch in range(args.epochs2):
-            np.random.shuffle(idx)
-            mu_z = mu_z[idx]
-            sd_z = sd_z[idx]
-            lr = args.lr2 if args.lr_epochs2 <= 0 else args.lr2 * math.pow(args.lr_fac2, math.floor(float(epoch) / float(args.lr_epochs2)))
-            epoch_loss = 0
-            for j in range(iteration_per_epoch):
-                mu_z_batch = mu_z[j*args.batch_size:(j+1)*args.batch_size]
-                sd_z_batch = sd_z[j*args.batch_size:(j+1)*args.batch_size]
-                z_batch = mu_z_batch + sd_z_batch * np.random.normal(0, 1, [args.batch_size, args.latent_dim])
-                loss = model.step(2, z_batch, lr, sess, writer, args.write_iteration)
-                epoch_loss += loss 
-            epoch_loss /= iteration_per_epoch
-
-            print('Date: {date}\t'
-                  'Epoch: [Stage 2][{0}/{1}]\t'
-                  'Loss: {2:.4f}.'.format(epoch, args.epochs2, epoch_loss, date=time.strftime('%Y-%m-%d %H:%M:%S')))
-        saver.save(sess, os.path.join(model_path, 'stage2'))
-    else:
-        saver.restore(sess, os.path.join(model_path, 'stage2'))
-
+    
+    
     # test dataset 
     x, side_length, channels = load_test_dataset(args.dataset, args.root_folder)
     np.random.shuffle(x)
     x = x[0:10000]
 
     # reconstruction and generation
-    img_recons = model.reconstruct(sess, x)
-    img_gens1 = model.generate(sess, 10000, 1)
-    img_gens2 = model.generate(sess, 10000, 2)
+    def reconstruct(x):
+        num_sample = np.shape(x)[0]
+        
+    
+    img_recons = reconstruct(x)
+    img_gens1 = generate(10000, 1)
+    img_gens2 = generate(10000, 2)
 
     img_recons_sample = stich_imgs(img_recons)
     img_gens1_sample = stich_imgs(img_gens1)
@@ -101,7 +48,6 @@ def main():
     plt.imsave(os.path.join(exp_folder, 'gen2_sample.jpg'), img_gens2_sample)
 
     # calculating FID score
-    tf.reset_default_graph()
     fid_recon = evaluate_fid_score(img_recons.copy(), args.dataset, args.root_folder, True)
     fid_gen1 = evaluate_fid_score(img_gens1.copy(), args.dataset, args.root_folder, True)
     fid_gen2 = evaluate_fid_score(img_gens2.copy(), args.dataset, args.root_folder, True)
@@ -159,23 +105,7 @@ if __name__ == '__main__':
     parser.add_argument('--base-dim', type=int, default=16)
     parser.add_argument('--fc-dim', type=int, default=512)
 
-    parser.add_argument('--epochs', type=int, default=400)
-    parser.add_argument('--lr', type=float, default=0.0001)
-    parser.add_argument('--lr-epochs', type=int, default=150)
-    parser.add_argument('--lr-fac', type=float, default=0.5)
-
-    parser.add_argument('--epochs2', type=int, default=800)
-    parser.add_argument('--lr2', type=float, default=0.0001)
-    parser.add_argument('--lr-epochs2', type=int, default=300)
-    parser.add_argument('--lr-fac2', type=float, default=0.5)
-    parser.add_argument('--cross-entropy-loss', default=False, action='store_true')
-    
-    parser.add_argument('--val', default=False, action='store_true')
-
     args = parser.parse_args()
     print(args)
-
-    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
 
     main()
---
+++ model/stage2/s2vae.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+
+class S2Encoder(nn.Module):
+	def __init__(
+		self, input_size, output_size, hidden_size, n_layers, batch_size, device
+	):
+		super(S2Encoder, self).__init__()
+		self.input_size = input_size
+		self.output_size = output_size
+		self.hidden_size = hidden_size
+		self.batch_size = batch_size
+		self.n_layers = n_layers
+		self.device = device
+		self.embed = nn.Linear(input_size, hidden_size)
+		self.fc = nn.ModuleList(
+			[nn.Linear(hidden_size, hidden_size) for i in range(self.n_layers)]
+		)
+		self.mu_net = nn.Linear(input_size + hidden_size, output_size)
+		self.logvar_net = nn.Linear(input_size + hidden_size, output_size)
+		self.hidden = self.init_hidden()
+	
+	def init_hidden(self, num_samples=None):
+        batch_size = num_samples if num_samples is not None else self.batch_size
+        hidden = []
+        for i in range(self.n_layers):
+            hidden.append(
+                torch.zeros(batch_size, self.hidden_size)
+                .requires_grad_(False)
+                .to(self.device)
+            )
+        self.hidden = hidden
+        return hidden
+	
+	def reparameterize(self, mu, logvar):
+        s_var = logvar.mul(0.5).exp_()
+        eps = s_var.data.new(s_var.size()).normal_()
+        return eps.mul(s_var).add_(mu), s_var
+	
+	def forward(self, inputs):
+		embedded = self.embed(inputs.view(-1, self.input_size))
+		h_in = embedded
+		for i in range(self.second_depth):
+			self.hidden[i] = nn.ReLu(self.fc[i](h_in, self.hidden[i]))
+			h_in = self.hidden[i]
+		h_in = torch.cat([inputs.view(-1, self.input_size), h_in], -1)
+		mu = self.mu_net(h_in)
+		logvar = self.logvar_net(h_in)
+		z, var = reparameterize(mu, logvar)
+		return mu, var, logvar, z
+		
+
+class S2Decoder(nn.Module):
+	def __init__(
+		self, input_size, output_size, hidden_size, n_layers, batch_size, device
+	):
+		super(S2Decoder, self).__init__()
+		self.input_size = input_size
+		self.output_size = output_size
+		self.hidden_size = hidden_size
+		self.batch_size = batch_size
+		self.n_layers = n_layers
+		self.device = device
+		self.embed = nn.Linear(input_size, hidden_size)
+		self.fc = nn.ModuleList(
+			[nn.Linear(hidden_size, hidden_size) for i in range(self.n_layers)]
+		)
+		
+		self.output = nn.Linear(input_size + hidden_size, output_size)
+		self.hidden = self.init_hidden()
+	
+	def init_hidden(self, num_samples=None):
+        batch_size = num_samples if num_samples is not None else self.batch_size
+        hidden = []
+        for i in range(self.n_layers):
+            hidden.append(
+                torch.zeros(batch_size, self.hidden_size)
+                .requires_grad_(False)
+                .to(self.device)
+            )
+        self.hidden = hidden
+        return hidden
+	
+	def forward(self, inputs):
+		embedded = self.embed(inputs.view(-1, self.input_size))
+		h_in = embedded
+		for i in range(self.n_layers):
+			self.hidden[i] = nn.ReLu(self.fc[i](h_in, self.hidden[i]))
+			h_in = self.hidden[i]
+		h_in = torch.cat([inputs.view(-1, self.input_size), h_id], -1)
+		z_hat = self.output(h_in)
+		
