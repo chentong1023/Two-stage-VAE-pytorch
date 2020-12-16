772201689112bb9a6ae7c501140aafff1a5f08a3
chentong1023 <chentong1023@qq.com>
2020-12-16 13:53:43 +0800

    eval with logs

--- model/losses.py
+++ model/losses.py
@@ -1,13 +1,13 @@
 import torch
 
-def kl_loss(mu, logsd):
-	kld = (torch.pow(mu, 2) + torch.pow(torch.exp(logsd), 2) - 2.0 * logsd - 1) / 2.0
-	return kld.sum()
+def kl_loss(mu, logvar):
+    kld = -0.5*(1+logvar-mu**2-logvar.exp()).sum(1).mean()
+    return kld
 	
-def gen_loss1(x, x_hat, loggamma_x):
-	relu = torch.nn.ReLU()
-	gen = x * torch.log(relu(x_hat)) + (1-x) * torch.log(relu(1-x_hat))
-	return -gen.sum()
+def gen_loss1(x, x_recon, loggamma_x):
+    n = x.size(0)
+    loss = F.binary_cross_entropy_with_logits(x_recon, x, size_average=False).div(n)
+    return loss
 
 def gen_loss2(x, x_hat, loggamma_x):
 	HALF_LOG_TWO_PI = 0.91893
