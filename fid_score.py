ad2e3b83b8109d004c4008c417dfe5124149cc6d
ColorlessX <1814620279@qq.com>
2020-12-17 18:22:13 +0800

    Update fid_score.py

--- fid_score.py
+++ fid_score.py
@@ -4,7 +4,7 @@ import os.path, sys, tarfile
 import numpy as np
 from scipy import linalg
 from six.moves import range, urllib
-import tensorflow as tf
+# import tensorflow as tf
 import numpy as np
 import os
 import gzip, pickle
@@ -22,77 +22,77 @@ from dataset import load_test_dataset
 
 import matplotlib.pyplot as plt
 
-# =================================================================================
-# tensorflow fid score
-
-def create_inception_graph(pth):
-    """Creates a graph from saved GraphDef file."""
-    # Creates graph from saved graph_def.pb.
-    with tf.gfile.FastGFile( pth, 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString( f.read())
-        _ = tf.import_graph_def( graph_def, name='FID_Inception_Net')
-
-
-# code for handling inception net derived from
-#   https://github.com/openai/improved-gan/blob/master/inception_score/model.py
-def _get_inception_layer(sess):
-    """Prepares inception net for batched usage and returns pool_3 layer. """
-    layername = 'FID_Inception_Net/pool_3:0'
-    pool3 = sess.graph.get_tensor_by_name(layername)
-    ops = pool3.graph.get_operations()
-    for op_idx, op in enumerate(ops):
-        for o in op.outputs:
-            shape = o.get_shape()
-            if shape._dims != []:
-              shape = [s.value for s in shape]
-              new_shape = []
-              for j, s in enumerate(shape):
-                if s == 1 and j == 0:
-                  new_shape.append(None)
-                else:
-                  new_shape.append(s)
-              o.__dict__['_shape_val'] = tf.TensorShape(new_shape)
-    return pool3
-#-------------------------------------------------------------------------------
-
-
-def get_activations_tf(images, sess, batch_size=50, verbose=False):
-    """Calculates the activations of the pool_3 layer for all images.
-
-    Params:
-    -- images      : Numpy array of dimension (n_images, hi, wi, 3). The values
-                     must lie between 0 and 256.
-    -- sess        : current session
-    -- batch_size  : the images numpy array is split into batches with batch size
-                     batch_size. A reasonable batch size depends on the disposable hardware.
-    -- verbose    : If set to True and parameter out_step is given, the number of calculated
-                     batches is reported.
-    Returns:
-    -- A numpy array of dimension (num images, 2048) that contains the
-       activations of the given tensor when feeding inception with the query tensor.
-    """
-    inception_layer = _get_inception_layer(sess)
-    d0 = images.shape[0]
-    if batch_size > d0:
-        print("warning: batch size is bigger than the data size. setting batch size to data size")
-        batch_size = d0
-    n_batches = d0//batch_size
-    n_used_imgs = n_batches*batch_size
-    pred_arr = np.empty((n_used_imgs,2048))
-    for i in range(n_batches):
-        if verbose:
-            print("\rPropagating batch %d/%d" % (i+1, n_batches), end="", flush=True)
-        start = i*batch_size
-        end = start + batch_size
-        batch = images[start:end]
-        pred = sess.run(inception_layer, {'FID_Inception_Net/ExpandDims:0': batch})
-        pred_arr[start:end] = pred.reshape(batch_size,-1)
-    if verbose:
-        print(" done")
-    return pred_arr
-#-------------------------------------------------------------------------------
-
+# # =================================================================================
+# # tensorflow fid score
+#
+# def create_inception_graph(pth):
+#     """Creates a graph from saved GraphDef file."""
+#     # Creates graph from saved graph_def.pb.
+#     with tf.gfile.FastGFile( pth, 'rb') as f:
+#         graph_def = tf.GraphDef()
+#         graph_def.ParseFromString( f.read())
+#         _ = tf.import_graph_def( graph_def, name='FID_Inception_Net')
+#
+#
+# # code for handling inception net derived from
+# #   https://github.com/openai/improved-gan/blob/master/inception_score/model.py
+# def _get_inception_layer(sess):
+#     """Prepares inception net for batched usage and returns pool_3 layer. """
+#     layername = 'FID_Inception_Net/pool_3:0'
+#     pool3 = sess.graph.get_tensor_by_name(layername)
+#     ops = pool3.graph.get_operations()
+#     for op_idx, op in enumerate(ops):
+#         for o in op.outputs:
+#             shape = o.get_shape()
+#             if shape._dims != []:
+#               shape = [s.value for s in shape]
+#               new_shape = []
+#               for j, s in enumerate(shape):
+#                 if s == 1 and j == 0:
+#                   new_shape.append(None)
+#                 else:
+#                   new_shape.append(s)
+#               o.__dict__['_shape_val'] = tf.TensorShape(new_shape)
+#     return pool3
+# #-------------------------------------------------------------------------------
+#
+#
+# def get_activations_tf(images, sess, batch_size=50, verbose=False):
+#     """Calculates the activations of the pool_3 layer for all images.
+#
+#     Params:
+#     -- images      : Numpy array of dimension (n_images, hi, wi, 3). The values
+#                      must lie between 0 and 256.
+#     -- sess        : current session
+#     -- batch_size  : the images numpy array is split into batches with batch size
+#                      batch_size. A reasonable batch size depends on the disposable hardware.
+#     -- verbose    : If set to True and parameter out_step is given, the number of calculated
+#                      batches is reported.
+#     Returns:
+#     -- A numpy array of dimension (num images, 2048) that contains the
+#        activations of the given tensor when feeding inception with the query tensor.
+#     """
+#     inception_layer = _get_inception_layer(sess)
+#     d0 = images.shape[0]
+#     if batch_size > d0:
+#         print("warning: batch size is bigger than the data size. setting batch size to data size")
+#         batch_size = d0
+#     n_batches = d0//batch_size
+#     n_used_imgs = n_batches*batch_size
+#     pred_arr = np.empty((n_used_imgs,2048))
+#     for i in range(n_batches):
+#         if verbose:
+#             print("\rPropagating batch %d/%d" % (i+1, n_batches), end="", flush=True)
+#         start = i*batch_size
+#         end = start + batch_size
+#         batch = images[start:end]
+#         pred = sess.run(inception_layer, {'FID_Inception_Net/ExpandDims:0': batch})
+#         pred_arr[start:end] = pred.reshape(batch_size,-1)
+#     if verbose:
+#         print(" done")
+#     return pred_arr
+# #-------------------------------------------------------------------------------
+#
 
 # =================================================================================
 # pytorch fid score
@@ -100,7 +100,6 @@ def get_activations_tf(images, sess, batch_size=50, verbose=False):
 def get_activations_pt(images, model, batch_size=64, dims=2048,
                     cuda=False, verbose=False):
     """Calculates the activations of the pool_3 layer for all images.
-
     Params:
     -- images      : Numpy array of dimension (n_images, 3, hi, wi). The values
                      must lie between 0 and 1.
@@ -195,20 +194,40 @@ def preprocess_real_images(real_images):
     return real_images
 
 
-def check_or_download_inception():
-    ''' Checks if the path to the inception file is valid, or downloads
-        the file if it is not present. '''
-    INCEPTION_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
-    model_file = 'classify_image_graph_def.pb'
-    if not os.path.exists(model_file):
-        print("Downloading Inception model")
-        from urllib import request
-        import tarfile
-        fn, _ = request.urlretrieve(INCEPTION_URL)
-        with tarfile.open(fn, mode='r') as f:
-            f.extract('classify_image_graph_def.pb')
-    return str(model_file)
+# def check_or_download_inception():
+#     ''' Checks if the path to the inception file is valid, or downloads
+#         the file if it is not present. '''
+#     INCEPTION_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+#     model_file = 'classify_image_graph_def.pb'
+#     if not os.path.exists(model_file):
+#         print("Downloading Inception model")
+#         from urllib import request
+#         import tarfile
+#         fn, _ = request.urlretrieve(INCEPTION_URL)
+#         with tarfile.open(fn, mode='r') as f:
+#             f.extract('classify_image_graph_def.pb')
+#     return str(model_file)
 
+
+# def evaluate_fid_score(fake_images, dataset, root_folder, norm=True):
+#     real_images, _, _ = load_test_dataset(dataset, root_folder)
+#     np.random.shuffle(real_images)
+#     real_images = real_images[0:10000]
+#     real_images = preprocess_real_images(real_images)
+#     fake_images = preprocess_fake_images(fake_images, norm)
+#
+#     inception_path = check_or_download_inception()
+#
+#     create_inception_graph(inception_path)
+#     sess = tf.InteractiveSession()
+#     sess.run(tf.global_variables_initializer())
+#
+#     print('calculating tf features...')
+#     real_out = get_activations_tf(real_images, sess)
+#     fake_out = get_activations_tf(fake_images, sess)
+#     fid_result = fid_score(real_out, fake_out)
+#
+#     return fid_result
 
 def evaluate_fid_score(fake_images, dataset, root_folder, norm=True):
     real_images, _, _ = load_test_dataset(dataset, root_folder)
@@ -216,16 +235,14 @@ def evaluate_fid_score(fake_images, dataset, root_folder, norm=True):
     real_images = real_images[0:10000]
     real_images = preprocess_real_images(real_images)
     fake_images = preprocess_fake_images(fake_images, norm)
+    
+    torch.hub.set_dir('./hub')
+    model = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=True)
 
-    inception_path = check_or_download_inception()
+    real_images_pt = np.transpose(real_images, (0, 3, 1, 2))/256
+    fake_images_pt = np.transpose(fake_images, (0, 3, 1, 2))/256
 
-    create_inception_graph(inception_path)
-    sess = tf.InteractiveSession()
-    sess.run(tf.global_variables_initializer())
-
-    print('calculating tf features...')
-    real_out = get_activations_tf(real_images, sess)
-    fake_out = get_activations_tf(fake_images, sess)
-    fid_result = fid_score(real_out, fake_out)
-
-    return fid_result
+    real_out_pt = get_activations_pt(real_images_pt, model)
+    fake_out_pt = get_activations_pt(fake_images_pt, model)
+    fid_result_pt = fid_score(real_out_pt, fake_out_pt)
+    return fid_result_pt
